317cae404f3b1289e067eac56968288baa9b822f
Nathan Clavel <nclavel@c1r4p13.42lehavre.fr>
2025-10-31 17:53:05 +0100

    First

--- lib/printf.h
+++ lib/printf.h
@@ -6,7 +6,7 @@
 /*   By: nclavel <nclavel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 13:51:35 by nclavel           #+#    #+#             */
-/*   Updated: 2025/10/29 13:53:04 by nclavel          ###   ########.fr       */
+/*   Updated: 2025/10/31 16:24:58 by nclavel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int	ft_printf(const char *str, ...);
-int	ft_hex(unsigned int nb, char *base);
-int	ft_putstr(char *str);
-int	ft_putnbr(long nb);
-int	ft_putchar(char c);
-int	ft_pourcent(void);
-int	ft_pointer(void *ptr);
-int	ft_unsputnbr(unsigned int nb);
+size_t	ft_printf(const char *str, ...);
+size_t	ft_hex(unsigned int nb, char *base);
+size_t	ft_putstr(char *str);
+size_t	ft_putnbr(long nb);
+size_t	ft_putchar(char c);
+size_t	ft_pourcent(void);
+size_t	ft_pointer(void *ptr);
+size_t	ft_unsputnbr(unsigned int nb);
+size_t	ft_ulhex(unsigned long nb, char *base)
+
 
 #endif
--- src/cspdiuxX%_1.c
+++ src/cspdiuxX%_1.c
@@ -6,21 +6,21 @@
 /*   By: nclavel <nclavel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 08:47:04 by nclavel           #+#    #+#             */
-/*   Updated: 2025/10/29 16:10:24 by nclavel          ###   ########.fr       */
+/*   Updated: 2025/10/31 16:23:15 by nclavel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib/printf.h"
 
-int	ft_putchar(char c)
+size_t	ft_putchar(char c)
 {
 	write(1, &c, 1);
 	return (1);
 }
 
-int	ft_putstr(char *str)
+size_t	ft_putstr(char *str)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (str[i])
@@ -31,15 +31,16 @@ int	ft_putstr(char *str)
 	return (i);
 }
 
-int	ft_putnbr(long nb)
+size_t	ft_putnbr(long nb)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	if (nb < 0)
 	{
-		write(1, "-", 1);
+		ft_putchar('-');
 		nb = -nb;
+		i++;
 	}
 	if (nb > 9)
 	{
@@ -55,20 +56,47 @@ int	ft_putnbr(long nb)
 	return (i);
 }
 
-int	ft_pointer(void *ptr)
+size_t	ft_pointer(void *ptr)
 {
-	int		size;
+	size_t		size;
 	long	cst;
 
-	cst = (long)ptr;
+	cst = (unsigned long)ptr;
 	size = 0;
 	if (ptr == NULL)
-		ft_putstr("(nil)");
+		size += ft_putstr("(nil)");
 	else
 	{
-		size = 2;
-		ft_putstr("0x");
-		size += ft_hex(cst, "0123456789abcdef");
+		size += ft_putstr("0x");
+		while (cst !=  0)
+		{
+			cst /= 10;
+		}
 	}
 	return (size);
+}
+
+size_t ft_ulhex(unsigned long nb, char *base)
+{
+	size_t i;
+
+	i = 0;
+	i = 0;
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+		i += ft_hex(nb, base);
+	}
+	if (nb > 16)
+	{
+		i += ft_hex(nb / 16, base);
+		i += ft_hex(nb % 16, base);
+	}
+	else
+	{
+		ft_putchar(base[nb]);
+		i++;
+	}
+	return (i);
 }
