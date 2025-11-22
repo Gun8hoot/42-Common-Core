adfcb02aa02949f4cb247e03411673e9c27b2b45
gun8hoot <gun8hoot@proton.me>
2025-11-22 22:59:37 +0100

    ahhhhhhhhhhhhhhh

--- src/mouvement_1.c
+++ src/mouvement_1.c
@@ -6,7 +6,7 @@
 /*   By: nclavel <nclavel@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 17:33:45 by nclavel           #+#    #+#             */
-/*   Updated: 2025/11/22 18:25:33 by nclavel          ###   ########.fr       */
+/*   Updated: 2025/11/22 22:22:28 by nclavel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,35 @@ void	stack_sa(t_stack *head_a)
 	save = head_a->next->value;
 	head_a->next->value = head_a->value;
 	head_a->value = save;
+}
+
+void	stack_sb(t_stack *head_b)
+{
+	int	save;
+
+	save = head_b->next->value;
+	head_b->next->value = head_b->value;
+	head_b->value = save;
+}
+
+void	stack_ss(t_stack *head_a, t_stack *head_b)
+{
+	stack_sa(head_a);
+	stack_sb(head_b);
+}
+
+void	stack_pa(t_stack **head_a, t_stack *head_b)
+{
+	t_stack *save;
+
+	save = head_b;
+	save->next = head_a;
+}
+
+void	stack_pb(t_stack **head_b, t_stack *head_a)
+{
+	t_stack *save;
+
+	save = head_a;
+	save->next = head_b;
 }
--- src/parser.c
+++ src/parser.c
@@ -6,13 +6,13 @@
 /*   By: nclavel <nclavel@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 13:41:13 by nclavel           #+#    #+#             */
-/*   Updated: 2025/11/22 19:23:56 by nclavel          ###   ########.fr       */
+/*   Updated: 2025/11/22 22:58:21 by nclavel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib/push_swap.h"
 
-t_stack	*created_stack(int	*tab)
+t_stack	*created_stack(int	*tab, size_t len)
 {
 	t_stack	*stack;
 	t_stack	*node;
@@ -20,7 +20,7 @@ t_stack	*created_stack(int	*tab)
 
 	i = 0;
 	stack = NULL;
-	while (tab[i])
+	while (i < len)
 	{
 		node = stack_new_node(tab[i]);
 		stack_add_back(&stack, node);
@@ -29,35 +29,72 @@ t_stack	*created_stack(int	*tab)
 	return (stack);
 }
 
-void	free_tab(char **tab)
+void	ft_free_alem(char **arr, int j)
 {
-	int	i;
+	if (j == 0)
+	{
+		while(arr[j])
+			j++;
+	}
+	j -= 1;
+	while (j >= 0)
+	{
+		free(arr[j]);
+		j--;
+	}
+	free(arr);
+}
 
-	i = 0;
-	while(tab[i])
-		free(tab[i++]);
+char **argv2arr(int argc, char **argv)
+{
+	char	**arr;
+	size_t	nb_tab;
+	size_t	nb_char;
+
+	nb_tab = 0;
+	arr = malloc(sizeof(char *) * argc);
+	if (!arr)
+		return(NULL);
+	while ((nb_tab + 1) < argc)
+	{
+		nb_char = 0;
+		arr = malloc(sizeof(char) * (ft_strlen(argv[nb_tab + 1]) + 1));
+		if (!arr)
+			return(ft_free_alem(arr, nb_tab), NULL);
+		while (argv[nb_tab + 1][nb_char])
+		{
+			arr[nb_tab][nb_char] = argv[nb_tab + 1][nb_char];			// SEGFAULT DE SES MORT ICI
+			nb_char++;
+		}
+		arr[nb_tab][nb_char] = '\0';
+		nb_tab++;
+	}
+	arr[nb_tab] = NULL;
+	return (arr);
 }
 
 t_stack	*parser(int argc, char **argv)
 {
 	char	**arr;
-	int		*tab;
 	t_stack *stack;
+	int		*tab;
+	size_t	len;
 
+	len = 0;
 	if (argc == 2)
-	{
 		arr = ft_split(argv[1], ' ');
-		if (!arr)
-			return (NULL);
-	}
 	else
-		arr = &argv[1];
+		arr = argv2arr(argc, argv);
+	if (!arr)
+			return (NULL);
 	tab = check_all_n_convert(arr);
 	if (tab == NULL)
-		return (free_tab(arr), NULL);
-	stack = created_stack(tab);
+		return (ft_free_alem(arr, 0), NULL);
+	while (arr[len])
+		len++;
+	stack = created_stack(tab, len);
 	free(tab);
-	free_tab(arr);
+	ft_free_alem(arr, 0);
 	if (!stack)
 		return (NULL);
 	return (stack);
