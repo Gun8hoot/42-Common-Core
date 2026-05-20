21a07d3615c0186e87d8041db12c66b046cdb18f
Gun8hoot <gun8hoot@proton.me>
2026-05-20 17:21:09 +0200

    MariaDB finally work

--- Makefile
+++ Makefile
@@ -3,17 +3,17 @@ DC = docker compose
 all: up
 
 up:
-	@printf "\e[0;32mStarting Inception services ...\e[0m\n"
+	@printf "\e[0;32m[?] Starting Inception services ...\e[0m\n"
 	@$(DC) up
 
 down:
-	@printf "\e[0;33mStoping Inception services ...\e[0m\n"
+	@printf "\e[0;33m[?] Stoping Inception services ...\e[0m\n"
 	@$(DC) down
 
 re:
-	@printf "\e[0;33mRebuilding Inception services ...\e[0m\n"
-	$(MAKE) down
-	$(DC) build
-	$(MAKE) up
+	@$(MAKE) down
+	@printf "\e[0;33m[?] Rebuilding Inception services ...\e[0m\n"
+	@$(DC) build
+	@$(MAKE) up
 
 .PHONY: up down re
--- sources/mariadb/entrypoint.sh
+++ sources/mariadb/entrypoint.sh
@@ -1,48 +1,69 @@
 #!/bin/sh
 
-function check ()
+bad_username="user usr root admin administrator mariadb maria db"
+bad_password="password 123456789 qwerty azerty pass"
+
+function check_file_exist()
 {
-	bad_username="user usr root admin administrator mariadb maria db"
-	bad_password="password 123456789 qwerty azerty pass"
-
-	if [ "$MARIADB_USER" == "" ]; then
-		printf "\x1b[31m[!] Missing environment 'MARIADB_USER' variable detected\n\x1b[0m" 1>&2
-		exit 1
-	elif [ "$MARIADB_PASSWD" == "" ]; then
-		printf "\x1b[31m[!] Missing environment 'MARIADB_PASSWD' variable detected\n\x1b[0m" 1>&2
-		exit 1
-	fi
-
-	for usr in $bad_username; do
-		if [ "$MARIADB_USER" == "$usr" ]; then
-			printf "\x1b[33m[!] The username '$usr' is common, this could lead to security issues\n\x1b[0m" 1>&2
+	local fileToFound=$1
+	for file in *; do
+		if [[ "$file" == "$fileToFound" ]]; then
+			return 1
 		fi
 	done
-	for passwd in $bad_password; do
-		if [ "$MARIADB_PASSWD" == "$passwd" ]; then
-			printf "\x1b[33m[!] The password '$passwd' for the user '$MARIADB_USER' is weak, this could lead to serious security issues\n\x1b[0m" 1>&2
-		fi
-	done
+	return 0
 }
 
+# --- GOES INTO THE MARIADB DATABASE LOCATION ---
 cd /var/lib/mysql
 
-check
-for file in *; do
-	if [ "$file" != "*" ]; then
-		echo $file
+# --- CHECK IF ENV VARS EXIST ---
+if [ "$MARIADB_USER" == "" ]; then
+	printf "\x1b[31m[!] Missing environment 'MARIADB_USER' variable detected\n\x1b[0m" 1>&2
+	exit 1
+elif [ "$MARIADB_PASSWD" == "" ]; then
+	printf "\x1b[31m[!] Missing environment 'MARIADB_PASSWD' variable detected\n\x1b[0m" 1>&2
+	exit 1
+elif [ "$MARIADB_DOMAIN" == "" ]; then
+	printf "\x1b[31m[!] Missing environment 'MARIADB_DOMAIN' variable detected\n\x1b[0m" 1>&2
+	exit 1
+fi
+
+# --- CHECK THE WEAKNESS OF THE USER AND PASSWORD ---
+for usr in $bad_username; do
+	if [ "$MARIADB_USER" == "$usr" ]; then
+		printf "\x1b[33m[!] The username '$usr' is common, this could lead to security issues\n\x1b[0m" 1>&2
+	fi
+done
+for passwd in $bad_password; do
+	if [ "$MARIADB_PASSWD" == "$passwd" ]; then
+		printf "\x1b[33m[!] The password '$passwd' for the user '$MARIADB_USER' is weak, this could lead to serious security issues\n\x1b[0m" 1>&2
 	fi
 done
 
-cat > init.sql << EOF
-CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER '$MARIADB_USER'@'127.0.0.1' IDENTIFIED BY '$MARIADB_PASSWD';
-GRANT ALL PRIVILEGES ON wordpress.* TO '$MARIADB_USER'@'127.0.0.1';
-FLUSH PRIVILEGES;
-EOF
+# --- INITIALIZED MARIADB ---
+printf "[+] Loading MariaDB\n" 1>&2
+check_file_exist "mysql.user"
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql 2>/dev/null
 
-rc-service mariadb start
-
+# --- STARTING MARIADB DAEMON ---
+printf "[+] Initialized MariaDB daemon\n" 1>&2
+/usr/bin/mariadbd-safe --datadir='/var/lib/mysql' &
 sleep 3
 
-mariadb -u root < ./init.sql
+# --- CONFIGURING THE DATABASE ---
+printf "[+] Checking for existant database ...\n" 1>&2
+check_file_exist "wordpress"
+if [ $? -eq 0 ]; then
+	cat > init.sql << EOF
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER '$MARIADB_USER'@'$MARIADB_DOMAIN' IDENTIFIED BY '$MARIADB_PASSWD';
+GRANT ALL PRIVILEGES ON wordpress.* TO '$MARIADB_USER'@'$MARIADB_DOMAIN';
+FLUSH PRIVILEGES;
+EOF
+	mariadb -u root < ./init.sql
+fi
+
+# --- WAITING UNTIL THE CONTAINER STOP (INFINITE LOOP) ---
+printf "[+] MariaDB successfully initialized\n" 1>&2
+wait
